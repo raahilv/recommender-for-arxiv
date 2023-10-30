d470c080f170723c8b7f691d98da3f3f88e269dd
EgeSayin <egesayin63@gmail.com>
2023-10-30 18:50:21 -0400

    Created RecommendHomeView and did the UI for computer science categories

---
+++ src/interface_adapeters/RecommendHome/RecommendHomeView.java
@@ -0,0 +1,55 @@
+package interface_adapeters.RecommendHome;
+
+import javax.swing.*;
+
+public class RecommendHomeView {
+    private JTabbedPane tabbedPanel;
+    private JPanel CS;
+    private JRadioButton computationalComplexityRadioButton;
+    private JRadioButton computersAndSocietyRadioButton;
+    private JRadioButton hardwareArchitectureRadioButton;
+    private JRadioButton databasesRadioButton;
+    private JRadioButton computationAndLanguageRadioButton;
+    private JRadioButton computationalGeometryRadioButton;
+    private JRadioButton computationalEngineeringFinanceAndRadioButton;
+    private JRadioButton computerVisionAndPatternRadioButton;
+    private JRadioButton digitalLibrariesRadioButton;
+    private JRadioButton discreteMathematicsRadioButton;
+    private JRadioButton dataStructuresAndAlgorithmsRadioButton;
+    private JRadioButton emergingTechnologiesRadioButton;
+    private JRadioButton formalLanguagesAndAutomataRadioButton;
+    private JRadioButton generalLiteratureRadioButton;
+    private JRadioButton multiagentSystemsRadioButton;
+    private JRadioButton performanceRadioButton;
+    private JRadioButton cryptographyAndSecurityRadioButton;
+    private JRadioButton graphicsRadioButton;
+    private JRadioButton multimediaRadioButton;
+    private JRadioButton programmingLanguagesRadioButton;
+    private JRadioButton computerScienceAndGameRadioButton;
+    private JRadioButton mathematicalSoftwareRadioButton;
+    private JRadioButton roboticsRadioButton;
+    private JRadioButton humanComputerInteractionRadioButton;
+    private JRadioButton numericalAnalysisRadioButton;
+    private JRadioButton symbolicComputationRadioButton;
+    private JRadioButton informationRetrievalRadioButton;
+    private JRadioButton neuralAndEvolutionaryComputingRadioButton;
+    private JRadioButton soundRadioButton;
+    private JRadioButton informationTheoryRadioButton;
+    private JRadioButton networkingAndInternetArchitectureRadioButton;
+    private JRadioButton softwareEngineeringRadioButton;
+    private JRadioButton machineLearningRadioButton;
+    private JRadioButton otherComputerScienceRadioButton;
+    private JRadioButton socialAndInformationNetworksRadioButton;
+    private JRadioButton logicInComputerScienceRadioButton;
+    private JRadioButton operatingSystemsRadioButton;
+    private JRadioButton systemsAndControlRadioButton;
+    private JRadioButton artificialIntelligenceRadioButton;
+    private JRadioButton distributedParallelAndClusterRadioButton;
+    private JPanel Economics;
+    private JPanel EEsSS;
+    private JPanel Math;
+    private JPanel QBiology;
+    private JPanel QFinance;
+    private JPanel Stat;
+    private JPanel Phy;
+}
