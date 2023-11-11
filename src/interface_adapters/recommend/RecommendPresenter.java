a465cc7a42262a64c5b72901952cff7f82baf8fd
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2023-11-11 17:38:46 -0500

    added starter code for *recommend presenter*

--- src/interface_adapters/recommend/RecommendPresenter.java
+++ src/interface_adapters/recommend/RecommendPresenter.java
@@ -1,4 +1,24 @@
 package interface_adapters.recommend;
 
-public class RecommendPresenter {
+import use_cases.recommend.RecommendOutputBoundary;
+import use_cases.recommend.RecommendOutputData;
+import interface_adapters.ViewManagerModel;
+
+public class RecommendPresenter implements RecommendOutputBoundary {
+
+    private final RecommendViewModel recommendViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public RecommendPresenter(RecommendViewModel recommendViewModel, ViewManagerModel viewManagerModel) {
+        this.recommendViewModel = recommendViewModel;
+        this.viewManagerModel = viewManagerModel;
+    }
+
+    public void prepareSuccessView(RecommendOutputData recommendOutputData) {
+
+    }
+    public void prepareFailView(String error) {
+
+    }
+
 }
