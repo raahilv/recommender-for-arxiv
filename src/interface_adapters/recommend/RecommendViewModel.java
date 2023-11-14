d419a7acf383f38879aac5451971e3f870b6aaa9
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2023-11-13 19:57:39 -0500

    added recommend presenter and recommen view model

--- src/interface_adapters/recommend/RecommendViewModel.java
+++ src/interface_adapters/recommend/RecommendViewModel.java
@@ -3,17 +3,34 @@ package interface_adapters.recommend;
 import interface_adapters.ViewModel;
 
 import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 
 public class RecommendViewModel extends ViewModel {
+    public static final String RECOMMEND_BUTTON_LABEL = "Recommend";
+    public static final String TITLE_LABEL = "Recommend View";
 
-    @Override
-    public void firePropertyChanged() {
+    private RecommendState state = new RecommendState();
 
+    public RecommendViewModel() {
+        super("recommend");
     }
 
-    @Override
-    public void addPropertyChangeListener(PropertyChangeListener listener) {
+    public void setState(RecommendState state) {
+        this.state = state;
+    }
 
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public RecommendState getState() {
+        return state;
     }
 
 }
