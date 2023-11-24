8041d5a16c02c06e57c4cb78c4c8706766a722a3
Raahil Vora <r.vora@mail.utoronto.ca>
2023-11-24 18:49:07 -0500

    For some reason, the previous commit didn't actually upstream the changes. Here's the new one

--- src/data_access/DataAccessFacade.java
+++ src/data_access/DataAccessFacade.java
@@ -1,4 +1,73 @@
 package data_access;
 
-public class DataAccessFacade {
+import entities.Author;
+import entities.ResearchPaper;
+import entities.User;
+import use_cases.login.LoginUserDataAccessInterface;
+import use_cases.recommend.RecommendDataAccessInterface;
+import use_cases.save.SaveDataAccessInterface;
+import use_cases.showAuthor.showAuthorDataAccessInterface;
+import use_cases.signup.SignupUserDataAccessInterface;
+import use_cases.vote.VoteDataAccessInterface;
+
+import java.util.List;
+
+public class DataAccessFacade implements LoginUserDataAccessInterface, RecommendDataAccessInterface, SaveDataAccessInterface, showAuthorDataAccessInterface, SignupUserDataAccessInterface, VoteDataAccessInterface {
+    LocalDataAccessObject lDAO;
+    ArxivDataAccessObject arxivDAO;
+
+    @Override
+    public boolean existsByName(String identifier) {
+        return lDAO.existsByName();
+    }
+
+    @Override
+    public void save(User user) {
+        lDAO.save(user)
+    }
+
+    @Override
+    public User get(String username) {
+        return lDAO.get(username);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return lDAO.existsByUsername(username);
+    }
+
+    @Override
+    public User getUser(String userName) {
+        return lDAO.getUser(userName);
+    }
+
+    @Override
+    public ResearchPaper getPaper(String paperName) {
+        return arxivDAO.getPaper(paperName);
+    }
+
+    @Override
+    public ResearchPaper getPaperById(String id) {
+        return arxivDAO.getPaperById(id);
+    }
+
+    @Override
+    public ResearchPaper getPaperByTitle(String title) {
+        return arxivDAO.getPaperByTitle(title);
+    }
+
+    @Override
+    public ResearchPaper getPaperByJournalReference(String journalReference) {
+        return arxivDAO.getPaperByJournalReference(journalReference);
+    }
+
+    @Override
+    public List<String> filterPapersByRootCategory(String parentCategory) {
+        return arxivDAO.filterPapersByRootCategory(parentCategory);
+    }
+
+    @Override
+    public List<ResearchPaper> getPapersbyAuthor(Author author) {
+        return arxivDAO.getPapersByAuthor(author);
+    }
 }
