cb782707d1947ad0bc35c46d52ab2a579116a1ce
Zherui Zhang <zzrmisaka@outlook.com>
2023-11-25 10:30:22 -0500

    Clear Recommend View, will implement in branch.

--- src/view/RecommendView.java
+++ src/view/RecommendView.java
@@ -1,19 +1,1 @@
 package view;
-
-import interface_adapters.save.SaveController;
-import interface_adapters.save.SaveViewModel;
-import interface_adapters.vote.VoteController;
-import interface_adapters.vote.VoteViewModel;
-
-import javax.swing.*;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
-
-public class RecommendView extends JPanel implements ActionListener, PropertyChangeListener {
-    private final SaveViewModel saveViewModel;
-    private final VoteViewModel voteViewModel;
-    private final SaveController saveController;
-    private final VoteController voteController;
-
-
-}
