fcedba5e7e31c6a644fa072fbb8f2ed85c9f2595
Zherui Zhang <zzrmisaka@outlook.com>
2023-11-25 07:15:09 -0500

    Implement VOTE state.

--- src/interface_adapters/vote/VoteState.java
+++ src/interface_adapters/vote/VoteState.java
@@ -4,15 +4,35 @@ public class VoteState {
     private String paperId = "";
     private boolean isUpvote = false;
     private boolean removeVote = false;
+    private String paperAlreadyUpvotedError = null;
+    private String paperAlreadyDownvotedError = null;
 
     public VoteState(VoteState copy) {
         this.paperId = copy.paperId;
         this.isUpvote = copy.isUpvote;
         this.removeVote = copy.removeVote;
+        this.paperAlreadyUpvotedError = copy.paperAlreadyUpvotedError;
+        this.paperAlreadyDownvotedError = copy.paperAlreadyDownvotedError;
     }
 
     public VoteState() {
 
+    }
+
+    public String getPaperAlreadyUpvotedError() {
+        return paperAlreadyUpvotedError;
+    }
+
+    public void setPaperAlreadyUpvotedError(String paperAlreadyUpvotedError) {
+        this.paperAlreadyUpvotedError = paperAlreadyUpvotedError;
+    }
+
+    public String getPaperAlreadyDownvotedError() {
+        return paperAlreadyDownvotedError;
+    }
+
+    public void setPaperAlreadyDownvotedError(String paperAlreadyDownvotedError) {
+        this.paperAlreadyDownvotedError = paperAlreadyDownvotedError;
     }
 
     public void setPaperId(String paperId) {
