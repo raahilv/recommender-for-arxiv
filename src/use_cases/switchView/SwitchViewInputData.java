eb479b68f201171a77eaa73e00ae62030599f833
Zherui Zhang <zzrmisaka@outlook.com>
2023-11-28 23:32:01 -0500

    Add and complete Switch View use cases, complete Switch View interface adapters.

---
+++ src/use_cases/switchView/SwitchViewInputData.java
@@ -0,0 +1,12 @@
+package use_cases.switchView;
+
+public class SwitchViewInputData {
+    final private String viewName;
+
+    public SwitchViewInputData(String viewName) {
+        this.viewName = viewName;
+    }
+    public String getViewName() {
+        return viewName;
+    }
+}
