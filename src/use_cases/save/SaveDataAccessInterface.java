35b80b6b8b6bf28caef31b7002d49363cb5614eb
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2023-12-01 20:37:02 -0500

    minor change to the method signature

--- src/use_cases/save/SaveDataAccessInterface.java
+++ src/use_cases/save/SaveDataAccessInterface.java
@@ -6,6 +6,6 @@ import entities.User;
 public interface SaveDataAccessInterface {
     User getUser(String username);
     ResearchPaper getPaper(String paperID);
-    void save(String username, ResearchPaper paper);
+    void savePaperToLibrary(String username, ResearchPaper paper);
 
 }
