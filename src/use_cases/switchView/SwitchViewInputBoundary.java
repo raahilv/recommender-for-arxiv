d38adaca40a4118fcafa7b9b831dda1805146526
Zherui Zhang <zzrmisaka@outlook.com>
2023-12-02 20:29:35 -0500

    Add Javadoc for Switch View use case and interface adapters.

--- src/use_cases/switchView/SwitchViewInputBoundary.java
+++ src/use_cases/switchView/SwitchViewInputBoundary.java
@@ -1,5 +1,10 @@
 package use_cases.switchView;
 
 public interface SwitchViewInputBoundary {
+
+    /**
+     * Execute Switch View use case, with input data for the use case.
+     * @param inputData the input data for Switch View use case
+     */
     void execute(SwitchViewInputData inputData);
 }
