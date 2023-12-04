4571105c70c1598eb341761df4fb2eaa5497abb0
Raahil Vora <r.vora@mail.utoronto.ca>
2023-12-03 21:45:53 -0500

    added non-description constructor for category

--- src/entities/Category.java
+++ src/entities/Category.java
@@ -14,6 +14,11 @@ public class Category {
         this.subcategory = subcategory;
         this.description = description;
     }
+    public Category(String rootCategory, String subcategory) {
+        this.rootCategory = rootCategory;
+        this.subcategory = subcategory;
+        this.description = "";
+    }
 
     public String getRootCategory() {
         return this.rootCategory;
