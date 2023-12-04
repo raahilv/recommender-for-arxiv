a778c11708eca09c9dfeb6243f24b24eedeb97ce
KaiwenZheng7609 <helloworld.zheng@mail.utoronto.ca>
2023-12-04 15:36:06 -0500

    ...

--- src/entities/CategoryFactory.java
+++ src/entities/CategoryFactory.java
@@ -7,7 +7,7 @@ public class CategoryFactory {
     public Category create(List<String> preferredCategoryRawData) {
         return new Category(
                 preferredCategoryRawData.get(0),  // root category
-                preferredCategoryRawData.get(1),  // subcategory
+                preferredCategoryRawData.get(0).toLowerCase() + "." + preferredCategoryRawData.get(1).toUpperCase(),  // subcategory
                 null  // category description
         );
     }
